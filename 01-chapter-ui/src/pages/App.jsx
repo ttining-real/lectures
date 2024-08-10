d4c2c21a55004e4298dceeb36d0453ac93086be5
ttining <ttining.real@gmail.com>
2024-08-10 10:59:47 +0900

    [학습] 컴포넌트에 props 전달하기 Part 5.
    - 리스트 렌더링

--- 01-chapter-ui/src/pages/App.jsx
+++ 01-chapter-ui/src/pages/App.jsx
@@ -1,10 +1,10 @@
-import Drink from '@/components/Drink';
+import List from '@/components/List';
 
 function App() {
   return (
     <div className="App">
       <h1>React 웹 앱</h1>
-      <Drink />
+      <List />
     </div>
   );
 }
