67c5cf96190e75c59e5471690ca1766d51d46c5d
ttining <ttining.real@gmail.com>
2024-08-27 08:52:58 +0900

    [onebite-react] section 6. 카운터 앱
    -UI 구현하기

---
+++ onebite-react/section06/src/components/Controller.jsx
@@ -0,0 +1,16 @@
+function Controller() {
+  return (
+    <>
+      <div style={{ display: "flex", gap: "4px" }}>
+        <button>-1</button>
+        <button>-10</button>
+        <button>-100</button>
+        <button>+100</button>
+        <button>+10</button>
+        <button>+1</button>
+      </div>
+    </>
+  );
+}
+
+export default Controller;
---
+++ onebite-react/section06/src/components/Viewer.jsx
@@ -0,0 +1,10 @@
+function Viewer() {
+  return (
+    <>
+      <div>현재 카운트:</div>
+      <h1>0</h1>
+    </>
+  );
+}
+
+export default Viewer;
