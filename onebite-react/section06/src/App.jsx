c5ca5769603c358a111efb301c5f19341b82554a
ttining <ttining.real@gmail.com>
2025-02-12 16:07:41 +0900

    [한입 리액트] 챌린지 Day. 12 - 카운터 앱
    - UI 구현하기

--- onebite-react/section06/src/App.jsx
+++ onebite-react/section06/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
 
 function App() {
-  return <h1>카운터 앱</h1>;
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
 }
 
 export default App;
