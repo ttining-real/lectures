3a585b92ee3987968a89bdc780ba708f9daeca56
ttining <ttining.real@gmail.com>
2025-02-12 18:13:49 +0900

    [한입 리액트] 챌린지 Day. 12 - 라이프사이클
    - 3. useEffect로 라이프사이클 제어하기
    - 4. React 개발자 도구 사용하기

--- onebite-react/section07/src/App.jsx
+++ onebite-react/section07/src/App.jsx
@@ -1,15 +1,30 @@
 import "./App.css";
 import Controller from "./components/Controller";
+import Even from "./components/Even";
 import Viewer from "./components/Viewer";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
   useEffect(() => {
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트 : 죽음
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -29,6 +44,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
