61dcf152f0831f62224f02eb7ddb571b0cab2044
ttining <ttining.real@gmail.com>
2025-02-13 20:35:23 +0900

    [한입 리액트] 챌린지 Day. 16 - 최적화
    - 최적화란?

---
+++ onebite-react/section10/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h3>오늘은 🗓️</h3>
+      <h1>{new Date().toDateString()}</h1>
+    </header>
+  );
+};
+
+export default Header;
