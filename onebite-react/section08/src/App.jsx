551f2013f656f5cdb8585855de679e3c4b089634
ttining <ttining.real@gmail.com>
2025-02-13 16:15:31 +0900

    [한입 리액트] 챌린지 Day. 13 - 투두리스트
    - Create - 투두 추가하기

--- onebite-react/section08/src/App.jsx
+++ onebite-react/section08/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
@@ -27,11 +27,23 @@ const mockData = [
 
 function App() {
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <div className='App'>
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- onebite-react/section08/src/components/Editor.jsx
+++ onebite-react/section08/src/components/Editor.jsx
@@ -1,10 +1,43 @@
+import { useState, useRef } from "react";
 import "./Editor.css";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+
+    onCreate(content);
+    setContent("");
+  };
+
   return (
     <div className='Editor'>
-      <input type='text' placeholder='새로운 Todo...' />
-      <button type='button'>추가</button>
+      <input
+        type='text'
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder='새로운 Todo...'
+      />
+      <button type='button' onClick={onSubmit}>
+        추가
+      </button>
     </div>
   );
 };
