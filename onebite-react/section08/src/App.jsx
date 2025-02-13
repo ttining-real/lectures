4fa4188274c67f2a2a6acbad6c4b12e6f0dcd341
ttining <ttining.real@gmail.com>
2025-02-13 15:57:54 +0900

    [한입 리액트] 챌린지 Day. 13 - 투두리스트
    - 기능 구현 준비하기

--- onebite-react/section08/src/App.jsx
+++ onebite-react/section08/src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "Next 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "TypeScript 공부하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className='App'>
-      <h1>To do List</h1>
       <Header />
       <Editor />
       <List />
