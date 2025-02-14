4bb853c457fb9a3ebf86ef97151aa1cd09d8a6fe
ttining <ttining.real@gmail.com>
2025-02-14 18:31:28 +0900

    [한입 리액트] 챌린지 Day. 19 감정 일기장
    - 공통 컴포넌트 구현하기

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -1,38 +1,46 @@
 import "./App.css";
 
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
+import Button from "./components/Button";
+import Header from "./components/Header";
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
-import { getEmotionImage } from "./util/get-emotion-image";
+// import { getEmotionImage } from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
-
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} alt='' />
-        <img src={getEmotionImage(2)} alt='' />
-        <img src={getEmotionImage(3)} alt='' />
-        <img src={getEmotionImage(4)} alt='' />
-        <img src={getEmotionImage(5)} alt='' />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log(`123번 버튼 클릭!`);
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log(`123번 버튼 클릭!`);
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log(`123번 버튼 클릭!`);
+        }}
+      />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
--- onebite-react/section12/src/pages/Home.jsx
+++ onebite-react/section12/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { useSearchParams } from "react-router-dom";
 const Home = () => {
   // /?value=hello
   const [params, setParams] = useSearchParams();
-  console.log(params.get("value"));
 
   return <div>Home</div>;
 };
