80ef72a1314fe506580050f6fa84c44579a5768d
ttining <ttining.real@gmail.com>
2025-02-15 12:53:40 +0900

    [한입 리액트] 챌린지 Day. 20 미션
    - 삭제하기 버튼 추가

---
+++ onebite-react/challenge/day20/src/components/DiaryItem.jsx
@@ -0,0 +1,40 @@
+import "./DiaryItem.css";
+
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { DiaryDispatchContext } from "../App";
+import { getEmotionImage } from "../util/get-emotion-image";
+import Button from "./Button";
+
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const { onDelete } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onClickDelete = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className='DiaryItem'>
+      <div
+        className={`img_section img_section_${emotionId}`}
+        onClick={() => nav(`/diary/${id}`)}
+      >
+        <img src={getEmotionImage(emotionId)} alt='' />
+      </div>
+      <div className='info_section' onClick={() => nav(`/diary/${id}`)}>
+        <div className='created_date'>
+          {new Date(createdDate).toLocaleDateString()}
+        </div>
+        <div className='content'>{content}</div>
+      </div>
+      <div className='button_section'>
+        <Button text={"수정하기"} onClick={() => nav(`/edit/${id}`)} />
+        <Button text={"삭제하기"} type={"NEGATIVE"} onClick={onClickDelete} />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryItem;
---
+++ onebite-react/challenge/day20/src/components/DiaryList.jsx
@@ -0,0 +1,52 @@
+import "./DiaryList.css";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Button from "./Button";
+import DiaryItem from "./DiaryItem";
+
+const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  const sortedData = getSortedData();
+
+  return (
+    <div className='DiaryList'>
+      <div className='menu_bar'>
+        <select name='' id='' onChange={onChangeSortType}>
+          <option value='latest'>최신순</option>
+          <option value='oldest'>오래된 순</option>
+        </select>
+        <Button
+          text={"새 일기 쓰기"}
+          type={"POSITIVE"}
+          onClick={() => nav(`/new`)}
+        />
+      </div>
+      <div className='list_wrapper'>
+        {sortedData.map((item) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DiaryList;
