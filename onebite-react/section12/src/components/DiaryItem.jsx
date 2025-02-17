01b2cd24e784254e425f86d3507df25c299070b7
ttining <ttining.real@gmail.com>
2025-02-17 17:34:30 +0900

    [한입 리액트] 챌린지 Day. 22
    - DiaryItem 컴포넌트 내 '삭제하기' 버튼 추가

--- onebite-react/section12/src/components/DiaryItem.jsx
+++ onebite-react/section12/src/components/DiaryItem.jsx
@@ -1,12 +1,19 @@
 import "./DiaryItem.css";
 
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { DiaryDispatchContext } from "../App";
 import { getEmotionImage } from "../util/get-emotion-image";
 import Button from "./Button";
 
 const DiaryItem = ({ id, emotionId, createdDate, content }) => {
   const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    onDelete(id);
+  };
 
   return (
     <div className='DiaryItem'>
@@ -24,6 +31,7 @@ const DiaryItem = ({ id, emotionId, createdDate, content }) => {
       </div>
       <div className='button_section'>
         <Button text={"수정하기"} onClick={() => nav(`/edit/${id}`)} />
+        <Button text={"삭제하기"} type={"NEGATIVE"} onClick={onClickDelete} />
       </div>
     </div>
   );
