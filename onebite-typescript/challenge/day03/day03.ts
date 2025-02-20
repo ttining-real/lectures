f6160a497213bb57b6cb1297a138fc1a233407d1
ttining <ttining.real@gmail.com>
2025-02-20 21:56:34 +0900

    [TypeScript] 챌린지 Day. 03 미션

---
+++ onebite-typescript/challenge/day03/day03.ts
@@ -0,0 +1,21 @@
+// * Quiz 1
+type Nums = number[];
+type Colors = string[];
+type Coords = [number, number][];
+type Info = [number, string][];
+
+// * Quiz 2
+type Course = {
+  name: string;
+  price: number;
+  student_cnt: number;
+  author: string;
+  related_courses: string[];
+};
+
+// * Quiz 3
+type User = {
+  name: string;
+  email: string;
+  [key: string]: string;
+};
