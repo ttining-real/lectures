d6c64818f5b3356030d500004182237b1e324ff0
ttining <ttining.real@gmail.com>
2025-03-31 11:34:52 +0900

    [Next.js] 섹션 2. Page Router 핵심 정리
    - 11. SSR 1. 소개 및 실습

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -1,11 +1,41 @@
 // CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import BookItem from "@/components/book-item";
 import books from "@/mock/books.json";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+// 페이지 역할을 하는 컴포넌트보다 먼저 실행되어, 컴포넌트에 필요한 데이터를 불러오는 함수
+// 마운트 시점에 한 번만 실행된다. (오직 서버 측에서만 실행)
+export const getServerSideProps = () => {
+  // 서버 측에서만 실행되기 때문에 터미널에서 확인 가능
+  // window.location;
+  // 브라우저 메서드는 사용할 수 없다.
+  console.log("서버 사이드 프롭스");
+
+  const data = "hello";
+
+  // getServerSideProps 함수의 리턴값은 props라는 객체 프로퍼티를 포함하는 단 하나의 객체여야 한다.
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+
+  // SSR이기 때문에 window 객체 사용 불가
+  // window.location;
+
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
