83492bf99d733d4bb1baac9e993645fde7a16c5e
ttining <ttining.real@gmail.com>
2025-12-20 13:49:38 +0900

    [section 2-6] 컴포넌트 이해하기 (2)

---
+++ vue3-master-basic/6-component/getting-started/src/components/BookComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <article class="book">
+    <div class="book_subtitle">제목</div>
+    <div class="book_title">HTML 강좌</div>
+  </article>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
