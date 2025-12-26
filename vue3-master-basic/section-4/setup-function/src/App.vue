e8076bdf6508037a14e6d4520cc40e6f2bf9247d
ttining <ttining.real@gmail.com>
2025-12-26 17:28:04 +0900

    [Vue.js] 기본편 섹션 4 - Setup 함수

---
+++ vue3-master-basic/section-4/setup-function/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>
+      {{ counter }}
+    </p>
+    <p>
+      {{ message }}
+    </p>
+    <button @click="increment">click!</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const counter = ref(0);
+    const message = ref('Hello Vue3');
+    const increment = () => {
+      counter.value++;
+    };
+
+    // 템플릿 및 기타 Options API hook에 노출
+    return {
+      counter,
+      message,
+      increment,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
