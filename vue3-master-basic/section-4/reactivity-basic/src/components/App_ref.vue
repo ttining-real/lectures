83ef413626eebbca9b6c854e2eaedc91bfb0febc
ttining <ttining.real@gmail.com>
2025-12-27 12:58:51 +0900

    [Vue.js] 기본편 섹션 4 - 반응형 기초 (Reactivity)
    - reactivity
    - ref
    - ref object vs array
    - toRefs & toRef
    - readonly

---
+++ vue3-master-basic/section-4/reactivity-basic/src/components/App_ref.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- <p>{{ message }}</p> -->
+    <!-- <p>{{ message.value }}</p> -->
+    <p>{{ message }}</p>
+    <button type="button" v-on:click="addMessage">add Click</button>
+  </div>
+</template>
+
+<script>
+// import { reactive, ref } from 'vue';
+import { ref } from 'vue';
+
+export default {
+  // setup() {
+  //   let message = reactive('Hello Vue!');
+  //   const addMessage = () => {
+  //     message = message + '!';
+  //   };
+
+  //   return { message, addMessage };
+  // },
+
+  // reactive로 primitive 값 변경하기
+  setup() {
+    // let message = reactive({ value: 'Hello Vue!' });
+
+    let message = ref('Hello Vue!');
+
+    const addMessage = () => {
+      message.value = message.value + '!';
+    };
+
+    console.log(message);
+
+    console.log('message: ', message.value);
+    console.log('message typeof: ', typeof message.value);
+
+    return { message, addMessage };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue3-master-basic/section-4/reactivity-basic/src/components/App_toRefs_toRef.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>author: {{ author }}</p>
+    <p>title: {{ title }}</p>
+  </div>
+</template>
+
+<script>
+import { reactive, toRef, toRefs } from 'vue';
+
+export default {
+  setup() {
+    // 반응형 상태 구조 분해하기 (Destructuring)
+    const book = reactive({
+      author: 'Vue Team',
+      year: '2020',
+      title: 'Vue 3 Guide',
+      description: '당신은 지금 바로 이 책을 읽습니다,,,',
+      price: '무료',
+    });
+
+    // 반응형을 잃어버림
+    // const { author, title } = book;
+
+    // 반응형 상태 동기화
+    // const { author, title } = toRefs(book);
+
+    // 하나만 가져오고 싶을 경우 toRef 사용
+    const author = toRef(book, 'author');
+    const title = toRef(book, 'title');
+
+    return { author, title, book };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
