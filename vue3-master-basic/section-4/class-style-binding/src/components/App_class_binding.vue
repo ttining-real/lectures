bb1e3ee9ef5ecbdeb86abd7595c34f358f8ed467
ttining <ttining.real@gmail.com>
2025-12-27 13:47:22 +0900

    [Vue.js] 기본편 섹션 4 - Class와 Style 바인딩

---
+++ vue3-master-basic/section-4/class-style-binding/src/components/App_class_binding.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <!-- <div class="text" :class="{ active: isActive, 'text-danger': hasError }">
+      텍스트 입니다.
+    </div> -->
+    <!-- <div class="text" :class="classObject">텍스트 입니다.</div> -->
+    <div
+      class="text"
+      :class="[isActive ? 'active-class' : 'class', errorClass, classObject]"
+    >
+      텍스트 입니다.
+    </div>
+    <button type="button" v-on:click="toggle">toggle</button>
+    <button type="button" v-on:click="hasError = !hasError">toggleError</button>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue';
+
+export default {
+  setup() {
+    const isActive = ref(true);
+    const hasError = ref(false);
+
+    // 클래스를 객체로 관리하기
+    // const classObject = reactive({
+    //   active: true,
+    //   'text-danger': true,
+    // });
+
+    // 액티브 상태가 여러 개일 때, computed 활용
+    const classObject = computed(() => {
+      return {
+        active: true && true,
+        'text-danger': true && true,
+        'text-blue': true,
+      };
+    });
+
+    const activeClass = ref('active');
+    const errorClass = ref('error');
+
+    const toggle = () => {
+      isActive.value = !isActive.value;
+    };
+
+    return { isActive, toggle, hasError, classObject, activeClass, errorClass };
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  font-weight: 900;
+}
+
+.text-danger {
+  color: red;
+}
+</style>
