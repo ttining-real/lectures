e6776b2dfc3677141d4d11945a9cb76e51ef5a41
ttining <ttining.real@gmail.com>
2025-12-28 12:40:11 +0900

    [Vue.js] 기본편 섹션 4 - 디렉티브

---
+++ vue3-master-basic/section-4/directives/src/components/App_directives.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <p v-text="msg"></p>
+    <p v-html="htmlStr"></p>
+    <p v-text="htmlStr"></p>
+    <!-- v-once : 업데이트가 한 번만 일어난다. -->
+    <p v-once>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const msg = ref('안녕하세요');
+    const htmlStr = ref('<strong>안녕!!!</strong>');
+
+    return { msg, htmlStr };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
