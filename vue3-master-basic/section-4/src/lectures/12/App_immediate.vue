32b2abdfc72bd1377a2977d74498ff0761a52999
ttining <ttining.real@gmail.com>
2025-12-31 15:52:43 +0900

    [Vue.js] 기본편 섹션 4 - watch, watchEffect

---
+++ vue3-master-basic/section-4/src/lectures/12/App_immediate.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <p>{{ reverseMessage }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  setup() {
+    const message = ref('Hello Vue3');
+    const reverseMessage = ref('');
+
+    // (2)
+    // const reverseFunction = newValue => {
+    //   console.log('즉시 실행함!');
+    //   reverseMessage.value = newValue.split('').reverse().join('');
+    // };
+
+    // (3)
+    const reverseFunction = () => {
+      console.log('즉시 실행함!');
+      reverseMessage.value = message.value.split('').reverse().join('');
+    };
+
+    // (1)
+    // watch(
+    //   message,
+    //   newValue => {
+    //     console.log('즉시 실행함!');
+
+    //     reverseMessage.value = newValue.split('').reverse().join('');
+    //   },
+    //   // 즉시 실행하고 싶은 경우, 3번 째 인자 사용
+    //   {
+    //     immediate: true,
+    //   }
+    // );
+
+    watch(message, reverseFunction);
+
+    // (2)
+    // reverseFunction(message.value);
+
+    // (3)
+    reverseFunction();
+
+    return { message, reverseMessage };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue3-master-basic/section-4/src/lectures/12/App_multiple_source_type.vue
@@ -0,0 +1,69 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { reactive, ref, watch } from 'vue';
+
+export default {
+  setup() {
+    const x = ref(0);
+    const y = ref(0);
+
+    const obj = reactive({
+      count: 0,
+    });
+
+    // watch(
+    //   () => x.value + y.value,
+    //   (sum, oldValue) => {
+    //     console.log('sum: ', sum);
+    //     console.log('oldValue: ', oldValue);
+    //   }
+    // );
+
+    // 배열
+    // watch([x, y], ([newX, newY]) => {
+    //   console.log(newX, newY);
+    // });
+
+    // 객체
+    // watch(obj, (newValue, oldValue) => {
+    //   console.log('newValue: ', newValue);
+    //   console.log('oldValue: ', oldValue);
+    // });
+    // 특정 요소를 감지하고 싶을 경우 getter 사용
+    // watch(
+    //   () => obj.count,
+    //   (newValue, oldValue) => {
+    //     console.log('newValue: ', newValue);
+    //   }
+    // );
+
+    // 깊은 감시
+    const person = reactive({
+      name: '홍길동',
+      age: 30,
+      hobby: '운동',
+      obj: {
+        count: 0,
+      },
+    });
+
+    // watch(person, newValue => {
+    //   console.log('newValue: ', newValue);
+    // });
+
+    watch(
+      () => person.obj,
+      newValue => {
+        console.log('newValue: ', newValue);
+      }
+    );
+
+    return { x, y, obj, person };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
